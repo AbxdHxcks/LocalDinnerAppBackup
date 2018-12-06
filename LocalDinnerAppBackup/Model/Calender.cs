5660962beb2bf1c393c4fb2b6debab085a60945a
AbxdHxcks <abedbadr1999@hotmail.com>
2018-12-06 14:46:31 +0100

    Created some classes

---
+++ LocalDinnerAppBackup/Model/Calender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDinnerAppBackup.Model
+{
+    class Calender
+    {
+        public int Dato { get; set; }
+        public int ActualNoOfSeats { get; set; }
+
+        public Calender(int dato, int actualNoOfSeats)
+        {
+            Dato = dato;
+            ActualNoOfSeats = actualNoOfSeats;
+        }
+    }
+}
